9eae550516ec2e0258083e78779ee25f4ceb85d3
hongyun.mhy <hongyun.mhy@alibaba-inc.com>
2016-04-22 22:36:23 +0800

    add ctci

--- src/com/idlefish/TestEntrance.java
+++ src/com/idlefish/TestEntrance.java
@@ -5,13 +5,17 @@ package com.idlefish;
  */
 public class TestEntrance {
     public static void main(String[] args) {
-        try{
-            System.out.println("111");
-            Object obj = null;
-            obj.hashCode();
-            System.out.println("111");
-        }finally {
-            System.out.print("xxx");
-        }
+//        try{
+//            System.out.println("111");
+//            Object obj = null;
+//            obj.hashCode();
+//            System.out.println("111");
+//        }finally {
+//            System.out.print("xxx");
+//        }
+        String str1 ="abcdef";
+        String str2 ="abcdef";
+        System.out.println(str1.hashCode());
+        System.out.println(str2.hashCode());
     }
 }
