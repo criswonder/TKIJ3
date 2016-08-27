baf67e2be633aa707d33c7e573a444403c33c7fc
hongyun.mhy <hongyun.mhy@alibaba-inc.com>
2016-08-27 15:21:34 +0800

    add excel explanation for question1_4

--- Chapter 1/Question1_1/Question.java
+++ Chapter 1/Question1_1/Question.java
@@ -16,6 +16,17 @@ public class Question {
 		}
 		return true;
 	}
+
+
+	public boolean isUniqueCharString(String str){
+		int mask = 0;
+		for (int i = 0; i < str.length(); i++) {
+			int bit = str.charAt(i) - 'a';
+			if((mask & 1 << bit) > 0)return false;
+			mask |=(1<<bit);
+		}
+		return true;
+	}
 	
 	public static boolean isUniqueChars2(String str) {
 		if (str.length() > 128) {
@@ -31,14 +42,32 @@ public class Question {
 	}
 	
 	public static void main(String[] args) {
-//		String[] words = {"abcde", "hello", "apple", "kite", "padle"};
-//		for (String word : words) {
-//			System.out.println(word + ": " + isUniqueChars(word) + " " + isUniqueChars2(word));
-//		}
-		
+		String[] words = {"aBBde", "hello", "apple", "kite", "padle"};
+		for (String word : words) {
+			System.out.println(word + ": " + isUniqueChars(word) + " " + isUniqueChars2(word));
+		}
+//
+		System.out.println(1 << -1);
 		System.out.println(1 << 31);
+
+		System.out.println(1 << -2);
+		System.out.println(1 << 30);
+//		System.out.println(1 >> 2);
+//
 //		System.out.println(1 << 33);
-		System.out.println(Integer.MAX_VALUE);
+//		System.out.println(Integer.MAX_VALUE);
+
+//		String print128 = print128();
+//		System.out.println(print128 + ": " + isUniqueChars(print128) + " " + isUniqueChars2(print128));
 	}
+
+	public static String print128(){
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i <128; i++) {
+			sb.append((char)i);
+		}
+		return sb.toString();
+	}
+
 
 }
